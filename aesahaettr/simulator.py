48061217c609bea91307334812b862fca3743e7c
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-05-05 22:58:53 -0700

    add notebooks and working on parser.

--- aesahaettr/simulator.py
+++ aesahaettr/simulator.py
@@ -34,23 +34,23 @@ golomb_dict = {0:[0], 1:[0,1], 2:[0,1,3],
                26:[0, 1, 33, 83, 104, 110, 124, 163, 185, 200, 203, 249, 251, 258, 314, 318, 343, 356, 386, 430, 440, 456, 464, 475, 487, 492],
                27:[0, 3, 15, 41, 66, 95, 97, 106, 142, 152, 220, 221, 225, 242, 295, 330, 338, 354, 382, 388, 402, 415, 486, 504, 523, 546, 553]}
 
-def get_basename(gorder, dx, df, nf, rspace, f0):
-        return f'HERA-III_dx{dx}_rspace{rspace}_G{gorder}_nf{nf}_df{df/1e3:.3f}kHz_f0{f0/1e6:.3f}'
+def get_basename(antenna_count, dish_size, df, nf, fractional_spacing, f0):
+        return f'HERA-III_dish_size{dish_size}_fractional_spacing{fractional_spacing}_G{antenna_count}_nf{nf}_df{df/1e3:.3f}kHz_f0{f0/1e6:.3f}'
 
-def run_simulation(gorder=10, dx=2.0, df=100e3, nf=200, rspace=1., dynamic_range_db=-5.5, f0=100e6, output_dir='./', NSIDE_GSM=256):
+def run_simulation(antenna_count=10, dish_size=2.0, df=100e3, nf=200, fractional_spacing=1., dynamic_range_db=-5.5, f0=100e6, output_dir='./', NSIDE_GSM=256):
 
     golomb_dict = {k:np.asarray(golomb_dict[k]) for k in golomb_dict}
-    antpos = golomb_dict[gorder] * dx * rspace
+    antpos = golomb_dict[antenna_count] * dish_size * fractional_spacing
     obs_freqs = np.linspace(f0, f0 + nf * df, nf)
     # write yaml file
-    telescope_yaml_dict = {'beam_paths': {i: {'type': 'airy'} for i in range(len(antpos))}, 'diameter': dx,
+    telescope_yaml_dict = {'beam_paths': {i: {'type': 'airy'} for i in range(len(antpos))}, 'diameter': dish_size,
                            'telescope_location': '(-30.721527777777847, 21.428305555555557, 1073.0000000046566)',
                            'telescope_name': 'HERA'}
-    telescope_yaml_name = os.path.join(output_dir, f'airy_config_dx{dx}.yaml')
+    telescope_yaml_name = os.path.join(output_dir, f'airy_config_dish_size{dish_size}.yaml')
     with open(telescope_yaml_name, 'w') as telescope_yaml_file:
         yaml.safe_dump(telescope_yaml_dict, telescope_yaml_file)
     # write csv file.
-    csv_name =  os.path.join(output_dir, f'layout_dx{dx}_rspace{rspace}_G{gorder}.csv')
+    csv_name =  os.path.join(output_dir, f'layout_dish_size{dish_size}_fractional_spacing{fractional_spacing}_G{antenna_count}.csv')
     lines = []
     lines.append('Name\tNumber\tBeamID\tE    \tN    \tU\n')
     for i, x  in enumerate(antpos):
@@ -64,7 +64,7 @@ def run_simulation(gorder=10, dx=2.0, df=100e3, nf=200, rspace=1., dynamic_range
                       'time': {'Ntimes': 1, 'duration_days': 0.0012731478148148147,
                                'integration_time': 11.0, 'start_time': 2457458.1738949567},
                       'polarization_array': [-5]}
-    basename = get_basename(gorder, dx, df, nf, rspace, f0)
+    basename = get_basename(antenna_count, dish_size, df, nf, fractional_spacing, f0)
     obs_param_yaml_name = os.path.join(output_dir, f'{basename}.yaml')
     with open(obs_param_yaml_name, 'w') as obs_param_yaml:
         yaml.safe_dump(obs_param_dict, obs_param_yaml)
@@ -103,10 +103,10 @@ def run_simulation(gorder=10, dx=2.0, df=100e3, nf=200, rspace=1., dynamic_range
     return gsm_simulator.uvdata, eor_simulator.uvdata
 
 
-def process_configuration(gorder=10, dx=2.0, df=100e3, nf=200, rspace=1.,
+def process_configuration(antenna_count=10, dish_size=2.0, df=100e3, nf=200, fractional_spacing=1.,
                           dynamic_range_db=-5.5, tol=1e-11, f0=100e6, output_dir='./',
                           buffer_multiplier=1.0, skip_existing=True):
-    basename = get_basename(gorder, dx, df, nf, rspace, f0)
+    basename = get_basename(antenna_count, dish_size, df, nf, fractional_spacing, f0)
     filtered_pbl_file = os.path.join(output_dir, basename + f'_bmult{buffer_multiplier:.2f}_tol{tol:.1e}_pbl.uvh5')
     filtered_ibl_file = os.path.join(output_dir, basename + f'_bmult{buffer_multiplier:.2f}_tol{tol:.1e}_ibl.uvh5')
     if (not os.path.exists(filtered_pbl_file) or not os.path.exists(filtered_ibl_file)) or not skip_existing:
@@ -118,8 +118,8 @@ def process_configuration(gorder=10, dx=2.0, df=100e3, nf=200, rspace=1.,
             uvd_gsm = UVData()
             uvd_gsm.read_uvh5(uvd_gsm_name)
         else:
-            uvd_eor, uvd_gsm = run_simulation(gorder=gorder,
-                                              dx=dx, df=df, nf=nf, rspace=rspace,
+            uvd_eor, uvd_gsm = run_simulation(antenna_count=antenna_count,
+                                              dish_size=dish_size, df=df, nf=nf, fractional_spacing=fractional_spacing,
                                               dynamic_range_db=dynamic_range_db, output_dir=output_dir)
         uvd_total = copy.deepcopy(uvd_eor)
         uvd_total.data_array = uvd_eor.data_array + uvd_gsm.data_array
@@ -129,18 +129,32 @@ def process_configuration(gorder=10, dx=2.0, df=100e3, nf=200, rspace=1.,
         uvd_filtered_ibl.write_uvh5(filtered_ibl_file, clobber=True)
 
 
-def generate_grid_params(r_spaces=None, dxs=None, nfs=None, gorders=None, dfs=None):
+def get_simulation_parser():
+    ap = argparse.ArgumentParser(description="Perform a 1-dimensional filtering simulation.")
+    ap.add_argument("--antenna_count", type=int, help="Number of antennas in array simulation.", default=10)
+    ap.add_argument("--max_bl_length", type=int, help="Maximum baseline length. Can be used to automatically determine the antenna count.", default=None)
+    ap.add_argument("--dish_diameter", type=float, default=4.0, help="Diameter of a single antenna element (uses Airy beam).")
+    ap.add_argument("--fractional_spacing", type=float, help="Distance between elements as a fraction of dish diameter.", default=1.0)
+    ap.add_argument("--Nfreqs", type=int, help="Number of frequency channels.", default=100)
+    ap.add_argument("--df", type=float, help="Frequency Channel width.", default=100e3)
+    ap.add_argument("--")
+
+
+
+
+
+def generate_grid_params(r_spaces=None, dish_sizes=None, nfs=None, antenna_counts=None, dfs=None):
 
     r_spaces = [1, 1.5, 2.]
-    dxs = [2.0, 4.0, 8.0, 14.0]
+    dish_sizes = [2.0, 4.0, 8.0, 14.0]
     nfs = [100, 200, 400]
-    gorders = [10, 15]
+    antenna_counts = [10, 15]
     dfs = [100e3, 200e3]
     param_combinations = []
 
-    for rspace in r_spaces:
-        for dx in dxs:
+    for fractional_spacing in r_spaces:
+        for dish_size in dish_sizes:
             for nf in nfs:
                 for df in dfs:
-                    for gorder in gorders:
-                        param_combinations.append({'rspace':rspace, 'dx':dx, 'nf':nf, 'df':df, 'gorder':gorder})
+                    for antenna_count in antenna_counts:
+                        param_combinations.append({'fractional_spacing':fractional_spacing, 'dish_size':dish_size, 'nf':nf, 'df':df, 'antenna_count':antenna_count})
