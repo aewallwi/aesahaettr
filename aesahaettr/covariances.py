4fd1498b82b41a3389e830878c495bcd589be952
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-05-06 20:49:16 -0700

    add unittests.

--- aesahaettr/covariances.py
+++ aesahaettr/covariances.py
@@ -3,7 +3,8 @@
 import numpy as np
 
 
-def cov_mat_simple(uvd, antenna_chromaticity=0.0, bl_cutoff=np.inf, order_by_bl_length=False):
+def cov_mat_simple(uvd=None, antenna_chromaticity=0.0, bl_cutoff_buffer=np.inf, order_by_bl_length=False,
+                   sim_param_yaml=None, return_bl_lens_freqs):
     """
     A covariance matrix that is both simple and flexible enough to describe
     the covariances within the wedge and simple antennas.
@@ -20,19 +21,40 @@ def cov_mat_simple(uvd, antenna_chromaticity=0.0, bl_cutoff=np.inf, order_by_bl_
         For two baselines with length b2 > b1
         baselines with (b1 + bl_cutoff_buffer) * max_freq < (b2 * min_freq)
         (max u of b1 < min u of b2) are set to have covariances of zero.
-        thus the larger the bl_cutoff_buffer, the fewer off-diag terms are set to zero.
+        thus the larger the bl_cutoff_buffer_buffer, the fewer off-diag terms are set to zero.
     order_by_bl_length: bool, optional
         If true, order columns and rows by increasing baseline length.
+    sim_param_yaml: str, optional
+        used if uvd is None.
+        parameters to generate a uvdata object to compute simple covariance matrix for.
+    return bl_len_freqs: bool, optional
+        if True, return vector of baseline lengths and frequencies..
 
     Returns
     -------
-    covmat: array-like
-        (Nbls * Nfreqs, Nbls * Nfreqs) filtering matrix that can be used to
-        cut between the fabric of the foregrounds and cosmological signal.
+    if bl_len_freqs:
+        blvals: array-like
+            (Nbls * Nfreqs) vector of baseline lengths for each cell.
+        nuvals: array-like
+            (Nbls * Nreqs) vector of frequency vals for each cell.
+        covmat: array-like
+            (Nbls * Nfreqs, Nbls * Nfreqs) filtering matrix that can be used to
+            cut between the fabric of the foregrounds and cosmological signal.
+    else:
+        covmat: array-like
+            (Nbls * Nfreqs, Nbls * Nfreqs) filtering matrix that can be used to
+            cut between the fabric of the foregrounds and cosmological signal.
 
     """
-    if np.any(~np.isclose(uvd.uvw_array[:, i])):
-        raise NotImplementedError("cov_mat_simple only currently supports 1d arrays oriented EW.")
+    if uvd is None
+        if sim_param_yaml is not None:
+            uvd, _, _ = initialize_uvdata_from_params(obs_param_yaml_name)
+            uvd = _complete_uvdata(uvd)
+        else:
+            raise ValueError("Must provide a uvdata object or sim_param_yaml")
+    for i in range(1, 2):
+        if np.any(~np.isclose(uvd.uvw_array[:, i], 0.0)):
+            raise NotImplementedError("cov_mat_simple only currently supports 1d arrays oriented EW.")
     # only works on a 1d array.
     data_inds = np.where(uvd.time_array == np.unique(uvd.time_array)[0])[0]
     # sort data inds by baseline length if we wish.
@@ -43,7 +65,7 @@ def cov_mat_simple(uvd, antenna_chromaticity=0.0, bl_cutoff=np.inf, order_by_bl_
     u_x, u_y = np.meshgrid(blvals * nuvals / 3e8, blvals * nuvals / 3e8)
     nu_x, nu_y = np.meshgrid(nuvals, nuvals)
     covmat = np.sinc(2 * (u_x - u_y)) * np.sinc(2 * antenna_chromaticity * (nu_x - nu_y))
-    if np.isfinite(bl_cutoff):
+    if np.isfinite(bl_cutoff_buffer):
         del nu_x, u_x
         del nu_y, u_y
         bl_x, bl_y = np.meshgrid(blvals, blvals)
--- aesahaettr/simulator.py
+++ aesahaettr/simulator.py
@@ -9,6 +9,7 @@ from aesahaettr import filter
 import tqdm
 import yaml
 from pygsm import GlobalSkyModel
+from pyuvdata import UVData
 from hera_sim.visibilities import vis_cpu
 from pyuvsim.simsetup import initialize_uvdata_from_params, _complete_uvdata
 
@@ -17,17 +18,17 @@ golomb_dict = {0:[0], 1:[0,1], 2:[0,1,3],
                6:[0,1,4,10,12,17], 7:[0,1,4,10,18,23,25],
                8:[0,1,4,9,15,22,32,34], 9:[0,1,5,12,25,27,35,41,44],
                10:[0,1,6,10,23,26,34,41,53,55],
-               11:[0, 1, 4, 13, 28, 33, 47, 54, 64, 70, 72]
-               12:[0, 2, 6, 24, 29, 40, 43, 55, 68, 75, 76, 85]
-               13:[0, 2, 5, 25, 37, 43, 59, 70, 85, 89, 98, 99, 106]
-               14:[0, 4, 6, 20, 35, 52, 59, 77, 78, 86, 89, 99, 122, 127]
+               11:[0, 1, 4, 13, 28, 33, 47, 54, 64, 70, 72],
+               12:[0, 2, 6, 24, 29, 40, 43, 55, 68, 75, 76, 85],
+               13:[0, 2, 5, 25, 37, 43, 59, 70, 85, 89, 98, 99, 106],
+               14:[0, 4, 6, 20, 35, 52, 59, 77, 78, 86, 89, 99, 122, 127],
                15:[0, 4, 20, 30, 57, 59, 62, 76, 100, 111, 123, 136, 144, 145, 151],
                16:[0, 1, 4, 11, 26, 32, 56, 68, 76, 115, 117, 134, 150, 163, 168, 177],
                17:[0, 5, 7, 17, 52, 56, 67, 80, 81, 100, 122, 138, 159, 165, 168, 191, 199],
                18:[0, 2, 10, 22, 53, 56, 82, 83, 89, 98, 130, 148, 153, 167, 188, 192, 205, 216],
                19:[0, 1, 6, 25, 32, 72, 100, 108, 120, 130, 153, 169, 187, 190, 204, 231, 233, 242, 246],
                20:[0, 1, 8, 11, 68, 77, 94, 116, 121, 156, 158, 179, 194, 208, 212, 228, 240, 253, 259, 283],
-               21:[0, 2, 24, 56, 77, 82, 83, 95, 129, 144, 179, 186, 195, 255, 265, 285, 293, 296, 310, 329, 333]
+               21:[0, 2, 24, 56, 77, 82, 83, 95, 129, 144, 179, 186, 195, 255, 265, 285, 293, 296, 310, 329, 333],
                22:[0, 1, 9, 14, 43, 70, 106, 122, 124, 128, 159, 179, 204, 223, 253, 263, 270, 291, 330, 341, 353, 356],
                23:[0, 3, 7, 17, 61, 66, 91, 99, 114, 159, 171, 199, 200, 226, 235, 246, 277, 316, 329, 348, 350, 366, 372],
                24:[0, 9, 33, 37, 38, 97, 122, 129, 140, 142, 152, 191, 205, 208, 252, 278, 286, 326, 332, 353, 368, 384, 403, 425],
@@ -35,28 +36,9 @@ golomb_dict = {0:[0], 1:[0,1], 2:[0,1,3],
                26:[0, 1, 33, 83, 104, 110, 124, 163, 185, 200, 203, 249, 251, 258, 314, 318, 343, 356, 386, 430, 440, 456, 464, 475, 487, 492],
                27:[0, 3, 15, 41, 66, 95, 97, 106, 142, 152, 220, 221, 225, 242, 295, 330, 338, 354, 382, 388, 402, 415, 486, 504, 523, 546, 553]}
 
+
 def get_basename(antenna_count=10, antenna_diameter=2.0, df=100e3, nf=200, fractional_spacing=1.0, f0=100e6):
     """Generate basename string for naming sim outputs and config yamls.
-
-    Parameters
-    ----------
-    See run_simulation docstring
-
-    Returns
-    -------
-    basename: str
-        basename string.
-    """
-        basename = f'HERA-III_antenna_diameter{antenna_diameter}_fractional_spacing{fractional_spacing}_G{antenna_count}_nf{nf}_df{df/1e3:.3f}kHz_f0{f0/1e6:.3f}'
-        return basename
-
-def run_simulation(antenna_count=10, antenna_diameter=2.0, df=100e3, nf=200, fractional_spacing=1., eor_fg_ratio=1e-5, f0=100e6,
-                   output_dir='./', nside_sky=256, clobber=False):
-    """End to end simulation of global sky-model with white noise EoR.
-
-    Simulate visibilities at a single time for a Golomb array of antennas located at the HERA site.
-    Uses the Global Sky Model (GSM) to compute foregrounds and simulates EoR signal as a white noise
-    healpix map. Antenna configuration is saved to
 
     Parameters
     ----------
@@ -75,12 +57,97 @@ def run_simulation(antenna_count=10, antenna_diameter=2.0, df=100e3, nf=200, fra
     fractional_spacing: float, optional
         spacing between antennas as fraction of antenna_diameter
         Default is 1.0
-    eor_fg_ratio: float, optional
-        ratio between stdev of eor and foregrounds over all healpix pixels.
-        default is 1e-5
     f0: float, optional
         minimum frequency to simulate (Hz)
         default is 100e6
+    Returns
+    -------
+    basename: str
+        basename string.
+    """
+    basename = f'HERA-III_antenna_diameter{antenna_diameter}_fractional_spacing{fractional_spacing}_G{antenna_count}_nf{nf}_df{df/1e3:.3f}kHz_f0{f0/1e6:.3f}'
+    return basename
+
+
+def intialize_telescope_yamls(output_dir='./', antenna_count=10, antenna_diameter=2.0, df=100e3, nf=200, fractional_spacing=1.0, f0=100e6):
+    basename = get_basename(antenna_count=antenna_count, antenna_diameter=antenna_diameter, df=df, nf=nf, fractional_spacing=fractional_spacing, f0=f0)
+    antpos = np.asarray(golomb_dict[antenna_count]) * antenna_diameter * fractional_spacing
+    telescope_yaml_dict = {'beam_paths': {i: {'type': 'airy'} for i in range(len(antpos))}, 'diameter': antenna_diameter,
+                           'telescope_location': '(-30.721527777777847, 21.428305555555557, 1073.0000000046566)',
+                           'telescope_name': 'HERA'}
+    obs_param_dict = {'freq':{'Nfreqs': int(nf), 'bandwidth': float(nf * df), 'start_freq': float(sky_freqs[0])},
+                      'telescope': {'array_layout': csv_name,
+                                    'telescope_config_name': telescope_yaml_name},
+                      'time': {'Ntimes': 1, 'duration_days': 0.0012731478148148147,
+                               'integration_time': 11.0, 'start_time': 2457458.1738949567},
+                      'polarization_array': [-5]}
+    telescope_yaml_name = os.path.join(output_dir, f'airy_config_antenna_diameter{antenna_diameter}.yaml')
+    if not os.path.exists(telescope_yaml_name) or clobber:
+        with open(telescope_yaml_name, 'w') as telescope_yaml_file:
+            yaml.safe_dump(telescope_yaml_dict, telescope_yaml_file)
+    # write csv file.
+    csv_name =  os.path.join(output_dir, f'layout_antenna_diameter{antenna_diameter}_fractional_spacing{fractional_spacing}_G{antenna_count}.csv')
+    lines = []
+    lines.append('Name\tNumber\tBeamID\tE    \tN    \tU\n')
+    for i, x  in enumerate(antpos):
+        lines.append(f'ANT{i}\t{i}\t{i}\t{x:.4f}\t{0:.4f}\t{0:.4f}\n')
+    if not os.path.exists(csv_name) or clobber:
+        with open(csv_name, 'w') as csv_file:
+            csv_file.writelines(lines)
+
+    obs_param_yaml_name = os.path.join(output_dir, f'{basename}.yaml')
+    if not os.path.exists(obs_param_yaml_name) or clobber:
+        with open(obs_param_yaml_name, 'w') as obs_param_yaml:
+            yaml.safe_dump(obs_param_dict, obs_param_yaml)
+    return obs_param_yaml_name, telescope_yamle_name, csv_name
+
+
+def initialize_simulation_uvdata(output_dir='./', clobber=False, keep_config_files_on_disk=False, **array_kwargs):
+    """Prepare configuration files and UVData to run simulation.
+
+    Parameters
+    ----------
+    output_dir: str, optional
+        directory to write simulation config files.
+    clobber: bool, optional
+        overwrite existing config files.
+    keep_config_files_on_disk: bool, optional
+        Keep config files on disk. Otherwise delete.
+        default is False.
+    array_kwargs: kwarg_dict, optional
+        array parameters. See get_basename for details.
+
+    Returns
+    -------
+    uvd: UVData object
+        blank uvdata file
+    """
+    # initialize telescope yaml
+    obs_param_yaml_name, telescope_yamle_name, csv_name = intialize_telescope_yamls(output_dir='./', **array_kwargs)
+    uvdata, beams, beam_ids = initialize_uvdata_from_params(obs_param_yaml_name)
+    # remove obs config files.
+    if not keep_config_files_on_disk:
+        for yt in [obs_param_yaml_name, csv_name, telescope_yaml_name]:
+            os.remove(yt)
+
+    beam_ids = list(beam_ids.values())
+    beams.set_obj_mode()
+    return _complete_uvdata(uvdata, inplace=False)
+
+
+def run_simulation(eor_fg_ratio=1e-5, output_dir='./', nside_sky=256, clobber=False, compress_by_redundancy=True,
+                   keep_config_files_on_disk=False, **array_config_kwargs):
+    """End to end simulation of global sky-model with white noise EoR.
+
+    Simulate visibilities at a single time for a Golomb array of antennas located at the HERA site.
+    Uses the Global Sky Model (GSM) to compute foregrounds and simulates EoR signal as a white noise
+    healpix map. Antenna configuration is saved to
+
+    Parameters
+    ----------
+    eor_fg_ratio: float, optional
+        ratio between stdev of eor and foregrounds over all healpix pixels.
+        default is 1e-5
     output_dir: str, optional
         path to directory to output simulation products
         deault is './'
@@ -91,6 +158,14 @@ def run_simulation(antenna_count=10, antenna_diameter=2.0, df=100e3, nf=200, fra
         Overwrite existing UVData files.
         Default is False. If False, read any existing files and return them
         rather then simulating them.
+    compress_by_redundancy: bool, optional
+        If True, compress uvdata outputs by redundant averaging.
+    output_dir: str, optional
+        directory to write simulation config files.
+    clobber: bool, optional
+        overwrite existing config files.
+    array_config_kwargs: kwarg_dict, optional
+        array parameters. See initialize_simulation_uvdata for details.
 
     Returns
     -------
@@ -100,52 +175,18 @@ def run_simulation(antenna_count=10, antenna_diameter=2.0, df=100e3, nf=200, fra
         UVData with visibilities of EoR emission.
 
     """
-    golomb_dict = {k:np.asarray(golomb_dict[k]) for k in golomb_dict}
-    antpos = golomb_dict[antenna_count] * antenna_diameter * fractional_spacing
-    sky_freqs = np.linspace(f0, f0 + nf * df, nf)
-    # write yaml file
-    telescope_yaml_dict = {'beam_paths': {i: {'type': 'airy'} for i in range(len(antpos))}, 'diameter': antenna_diameter,
-                           'telescope_location': '(-30.721527777777847, 21.428305555555557, 1073.0000000046566)',
-                           'telescope_name': 'HERA'}
-    telescope_yaml_name = os.path.join(output_dir, f'airy_config_antenna_diameter{antenna_diameter}.yaml')
-    if not os.path.exists(telescope_yaml_name):
-        with open(telescope_yaml_name, 'w') as telescope_yaml_file:
-            yaml.safe_dump(telescope_yaml_dict, telescope_yaml_file)
-    # write csv file.
-    csv_name =  os.path.join(output_dir, f'layout_antenna_diameter{antenna_diameter}_fractional_spacing{fractional_spacing}_G{antenna_count}.csv')
-    lines = []
-    lines.append('Name\tNumber\tBeamID\tE    \tN    \tU\n')
-    for i, x  in enumerate(antpos):
-        lines.append(f'ANT{i}\t{i}\t{i}\t{x:.4f}\t{0:.4f}\t{0:.4f}\n')
-    if not os.path.exists(csv_name):
-        with open(csv_name, 'w') as csv_file:
-            csv_file.writelines(lines)
-    # generate obs param dict and csv file
-    gsm_file_name = basename + '_gsm.uvh5'
-    eor_file_name = basename + f'_eor_{eor_fg_ratio:.1f}dB.uvh5'
-    obs_param_dict = {'freq':{'Nfreqs': int(nf), 'bandwidth': float(nf * df), 'start_freq': float(sky_freqs[0])},
-                      'telescope': {'array_layout': csv_name,
-                                    'telescope_config_name': telescope_yaml_name},
-                      'time': {'Ntimes': 1, 'duration_days': 0.0012731478148148147,
-                               'integration_time': 11.0, 'start_time': 2457458.1738949567},
-                      'polarization_array': [-5]}
-    basename = get_basename(antenna_count, antenna_diameter, df, nf, fractional_spacing, f0)
-    obs_param_yaml_name = os.path.join(output_dir, f'{basename}.yaml')
-    if not os.path.exists(obs_param_yaml_name):
-        with open(obs_param_yaml_name, 'w') as obs_param_yaml:
-            yaml.safe_dump(obs_param_dict, obs_param_yaml)
     # only perform simulation if clobber is true and gsm_file_name does not exist and eor_file_name does not exist:
     # generate GSM cube
+    basename = initialize_simulation_uvdata(**array_config_kwargs)
+    gsm_file_name = basename + f'compressed_{compress_by_redundancy}_gsm.uvh5'
+    eor_file_name = basename + f'compressed_{compress_by_redundancy}_eor_{eor_fg_ratio:.1f}dB.uvh5'
     if not os.path.exists(gsm_file_name) or clobber:
-        uvdata, beams, beam_ids = initialize_uvdata_from_params(obs_param_yaml_name)
-        beam_ids = list(beam_ids.values())
-        beams.set_obj_mode()
-        _complete_uvdata(uvdata, inplace=True)
         gsm = GlobalSkyModel(freq_unit='Hz')
         NPIX_GSM = hp.nside2npix(nside_sky)
         gsmcube = np.zeros((nf, NPIX_GSM))
         rot=hp.rotator.Rotator(coord=['G', 'C'])
-        for fnum,f in enumerate(sky_freqs):
+        uvdata = initialize_simulation_uvdata(output_dir=output_dir, clobber=clobber, **array_config_kwargs)
+        for fnum, f in enumerate(uvdata.freq_array[0]):
             mapslice = gsm.generate(f)
             mapslice = hp.ud_grade(mapslice, nside_sky)
             # convert from galactic to celestial
@@ -156,28 +197,33 @@ def run_simulation(antenna_count=10, antenna_diameter=2.0, df=100e3, nf=200, fra
         gsm_simulator = vis_cpu.VisCPU(uvdata=uvdata, sky_freqs=sky_freqs, beams=beams, beam_ids=beam_ids, sky_intensity=gsmcube)
         gsm_vis = gsm_simulator.simulate()
         gsm_simulator.uvdata.vis_units='Jy'
-        gsm_simulator.uvdata.write_uvh5(gsm_file_name, clobber=True)
+        uvd_gsm = gsm_simulator.uvdata
+        if compress_by_redundancy:
+            # compress with quarter wavelength tolerance.
+            uvd_gsm.compress_by_redundancy(tol = 0.25 * 3e8 / uvd_gsm.freq_array.max())
+        uvd_gsm.write_uvh5(gsm_file_name, clobber=True)
         uvd_gsm = gsm_simulator.uvdata
     else:
         uvd_gsm = UVData()
         uvd_gsm.read(gsm_file_name)
     # only do eor cube if file does not exist.
     if not os.path.exists(eor_file_name) or clobber:
-        # define eor cube with random noise.
-        eorcube = np.random.randn(nf, hp.nside2npix(nside_sky)) + 1j * np.random.randn(nf, hp.nside2npix(nside_sky))
-        eorcube *= np.std(gsmcube) / np.std(eorcube) * eor_fg_ratio
         # initialize simulator
-        uvdata, beams, beam_ids = initialize_uvdata_from_params(obs_param_yaml_name)
-        beam_ids = list(beam_ids.values())
-        beams.set_obj_mode()
+        uvdata = initialize_simulation_uvdata(output_dir=output_dir, clobber=clobber, **array_config_kwargs)
+        # define eor cube with random noise.
+        eorcube = np.random.randn(uvdata.Nfreqs, hp.nside2npix(nside_sky)) + 1j * np.random.randn(uvdata.Nfreqs, hp.nside2npix(nside_sky))
         eor_simulator = vis_cpu.VisCPU(uvdata=uvdata, sky_freqs=sky_freqs, beams=beams, beam_ids=beam_ids, sky_intensity=eorcube)
         # simulator
         eor_vis = eor_simulator.simulate()
         # set visibility units.
         eor_simulator.uvdata.vis_units='Jy'
         # write out
-        eor_simulator.uvdata.write_uvh5(eor_file_name, clobber=True)
         uvd_eor = eor_simulator.uvdata
+        if compress_by_redundancy:
+            # compress with quarter wavelength tolerance.
+            uvd_gsm.compress_by_redundancy(tol = 0.25 * 3e8 / uvd_eor.freq_array.max())
+        uvd_eor.data_array *= np.mean(np.abs(uvd_gsm.data_array) ** 2.) / np.mean(np.abs(uvd_eor.data_array.real) ** 2.) * eor_fg_ratio
+        uvd_eor.write_uvh5(eor_file_name, clobber=True)
     else:
         # just read in if clobber=False and file already exists.
         uvd_eor = UVData()
